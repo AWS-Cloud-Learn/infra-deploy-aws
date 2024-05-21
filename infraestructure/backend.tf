852fa94faea9f6ed84cef6c97d3fce9d14bfb17b
David Placido <david.placido.matrix@gmail.com>
2024-05-21 01:01:46 -0500

    update region on banckend

--- infraestructure/backend.tf
+++ infraestructure/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "mi-terraform-bucket"
     key            = "mi-proyecto/dev/terraform.tfstate"
-    region         = var.region
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "mi-terraform-lock-table"
   }
